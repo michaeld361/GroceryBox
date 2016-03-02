57120de86b279566b8135c1a955d1fe51ec6f955
Michael Down <micke_obi_361@hotmail.co.uk>
2016-03-02 16:54:08 +0000

    fixed issue whereby list items are duplicated

--- public/js/listEngine.js
+++ public/js/listEngine.js
@@ -1,19 +1,12 @@
 
-var user = 0;
-var userID = "";
-var output;
-var groupID = 0;
-var myName = '';
-var myGroups = '';
-var groupString = '';
-var userDisplayName = '';
+
 var userGroup = '';
+var groupID = '';
+var myDataRef = new Firebase('https://todofyp.firebaseio.com/' + 'listItems/');
+var userID = '';
 
-var myDataRef;
-var listRef;
-var groupRef;
-// Create a callback which logs the current auth state
 
+//Login 
 function authDataCallback(authData) {
   if(authData) {
     console.log("User " + authData.uid + " is logged in with " + authData.provider);
@@ -26,18 +19,29 @@ function authDataCallback(authData) {
     window.location.href = "https://todofyp.firebaseapp.com/login.html";
   }
 }
-
 function logout()
 {
     ref45.unauth();
     window.location.href = "https://todofyp.firebaseapp.com/login.html";
 }
 
-
-
 // Register the callback to be fired every time auth state changes
 var ref45 = new Firebase("https://todofyp.firebaseio.com");
 ref45.onAuth(authDataCallback);
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
 
 
 
@@ -59,9 +63,17 @@ ref.on("value", function(snapshot) {
 
 }
 
+
+
+
+
+
+
+
+
+// get groups
 function getSubscribedGroups(userID)
 {
-
 var windowWidth = $(window).width();
 var myDetails = userID;
  var ref = new Firebase('https://todofyp.firebaseio.com/users/' + myDetails);
@@ -93,128 +105,102 @@ ref.on("value", function(snapshot) {
 
 }
 
-function changeGroup(id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeGroup(groupDiv)
 {
 
-    $('#messageInput').val('');
-    document.getElementById('listItems').innerHTML = "";
-    localStorage.setItem('groupID', id);
-    storeGroupChange();
-    updateGroupSelected(id);
+    localStorage.setItem('groupID', groupDiv);
+    console.log('group switched to ' + groupDiv);
+    userGroup = groupDiv;
+    spyItem();
+
 }
 
-function createGroup()
-{
-  var myDetails = userID;
-  var newGroupName = document.getElementById('createGroup').value;
-  var ref = new Firebase('https://todofyp.firebaseio.com/users/' + myDetails);
-  var newGroup = groupString + ',' + newGroupName;
-  ref.child('groupID').set(newGroup);
-  alert('group added');
 
 
-}
-
-
-
-function storeGroupChange()
-{
 if(localStorage.getItem('groupID') !== null || localStorage.getItem('groupID') !== 0)
 {
   groupID = localStorage.getItem('groupID');
-  console.log('bro: ' + groupID);
-  document.getElementById('groupHeader').innerHTML = groupID;
-}
+  console.log('Your Group: ' + groupID);
 }
 
-
-    // var userGroup = groupID.toString();
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+      updateGroupSelected();
+        }
+      });
 
 function updateGroupSelected()
 {
-    console.log(groupID + ' --lookie');
-      userGroup = groupID.toString();
-      myDataRef = new Firebase('https://todofyp.firebaseio.com/');
-      listRef = myDataRef.child("listItems");
-      groupRef = listRef.child(userGroup);
-      var defaultStatus = 'nrl';
-      $('#messageInput').keypress(function (e) {
-        if (e.keyCode == 13) {
-          
+
+      console.log('Your Group2: ' + userGroup);
+      var groupRef = new Firebase(myDataRef + '/' + userGroup);
+      console.log('groupRef: ' + groupRef);
           var name = userDisplayName;
           var text = $('#messageInput').val();
+          var defaultStatus = 'nrl';
           if(text != '')
           {
           var currentTime = Firebase.ServerValue.TIMESTAMP;
-          groupRef.push({name: name, text: text, groupID: userGroup, status: defaultStatus, time: currentTime});
-          $('#messageInput').val('');
+          groupRef.push({name: name, text: text, groupID: userGroup, status: defaultStatus, time: currentTime}, onComplete);
+          console.log('Item Added: ' + text);
           text = '';
-
-
+          $('#messageInput').val('');
           }
-        }
-      });
-      var onValueChange = groupRef.on('child_added', function(snapshot) {
-        var message = snapshot.val();
-        var key = snapshot.key();
-        displayChatMessage(message.name, message.text, key);
-        ItemsAddedByOthers(message.time, message.text, key);
+spyItem();
 
-      });
+}
 
+
+
+function onComplete()
+{
+  console.log('SAVED IN DB');
+}
+
+
+    
       
 
 
+function spyItem()
+{
+
+        document.getElementById('listItems').innerHTML = "";
+        var myRef = new Firebase(myDataRef + '/' + userGroup);
+        myRef.on('child_added', function(snapshot) {
+
+            console.log('item added to ' + userGroup);
+            var message = snapshot.val();
+            var key = snapshot.key();
+            displayChatMessage(message.name, message.text, key);
+            ItemsAddedByOthers(message.time, message.text, key);
+            
+          });
 
 
- groupRef.on('child_removed', function(snapshot) {
+           myRef.on('child_removed', function(snapshot) {
         var message = snapshot.val();
         var key = snapshot.key();
         removeListItemUI(message.name, message.text, key);
- });
+       });
 
+            
+        }
 
-
-// END
-
-
-}
-
-
-
-
-var amOnline = new Firebase('https://todofyp.firebaseio.com/.info/connected');
-var userRef = new Firebase('https://todofyp.firebaseio.com/presence/' + userID);
-amOnline.on('value', function(snapshot) {
-  if (snapshot.val()) {
-    userRef.onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
-    //userRef.set(true);
-  }
-});
-
-function ItemsAddedByOthers(timeAdded, text, key)
-{
-  var timeOfAdd = timeAdded;
-    var yourMessage = text;
-    var itemID = key;
- // alert(timeOfAdd + " -- " + yourMessage);
-var userRef = new Firebase('https://todofyp.firebaseio.com/presence/' + userID);
-userRef.on('value', function(snapshot) {
-  if (snapshot.val() < timeOfAdd) {
-    // User is online, update UI.
-
-    document.getElementById(key).innerHTML += '<div class="newIcon">Recently Added</div>';
-
-
-
-    //alert('Newly Added: ' + yourMessage);
-  } else {
-    // User logged off at snapshot.val() - seconds since epoch.
-  }
-});
-
-
-}
 
 
 
@@ -224,81 +210,6 @@ function displayChatMessage(name, text, key) {
         $('</div>').text();
       }
 
-//alert user when item set to urgent
-
- var ref67 = new Firebase('https://todofyp.firebaseio.com/listItems/');
-// Attach an asynchronous callback to read the data at our posts reference
-ref67.on("child_changed", function(snapshot) {
-  var itemStatus = snapshot.key();
-  if (groupString.indexOf(itemStatus) >= 0)
-{
- var groupUrgentRef = new Firebase('https://todofyp.firebaseio.com/listItems/' + itemStatus);
- groupUrgentRef.on("child_changed", function(snapshot) {
-  var itemStatus = snapshot.val();
-  var hitmebaby = itemStatus.status;
-  if(hitmebaby != 'nrl')
-  {
-  alert('Item marked at urgent');
-}
-})
-  }
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
-
-
-
-function removeListItemUI(name, text, key)
-      {
-       // $('.listItem').child(text).remove();
-       console.log('key: ' + key);
-        console.log('item removed: ' + text);
-        $( "#" + key ).css( "text-decoration", "line-through" );
-        $( "#" + key ).css( "opacity", "0.6" );
-      }
-
-
-      
-function doubletap(id) {
-
-//$("#" + id).on("tap",function(){
-
-
-      console.log(userGroup + " --V-- " + id);
-        var deleteRef = new Firebase("https://todofyp.firebaseio.com/listItems/" + userGroup + "/" + id);
-        deleteRef.remove();
-
-
-//});
-}
-
-
-
-
-
-            function removeListItemDB(id)
-      {
-
- doubletap(id);
-
-}
-
- $('#createGroup').keypress(function (e) {
-        if (e.keyCode == 13) {
-            createGroup();
-            $('#createGroup').val('');
-}
-})
-
-
- $('#createGroupDesktop').keypress(function (e) {
-        if (e.keyCode == 13) {
-            createGroup();
-            $('#createGroupDesktop').val('');
-}
-})
 
 
 
@@ -310,55 +221,9 @@ function doubletap(id) {
 
 
 
-/*
 
 
-
-function getListUpdates(id)
-{
-      var groupID = id;
-      document.getElementById('listItems').innerHTML = "";
-      var userGroup = groupID.toString();
-      var myDataRef = new Firebase('https://todofyp.firebaseio.com/');
-      var listRef = myDataRef.child("listItems");
-      var groupRef = listRef.child(userGroup);
-      var defaultStatus = 'nrl';
-      $('#messageInput').keypress(function (e) {
-        if (e.keyCode == 13) {
-          
-          var name = userDisplayName;
-          var text = $('#messageInput').val();
-          var currentTime = Firebase.ServerValue.TIMESTAMP;
-          groupRef.push({name: name, text: text, groupID: userGroup, status: defaultStatus, time: currentTime});
-          $('#messageInput').val('');
-        }
-      });
-      groupRef.on('child_added', function(snapshot) {
-        var message = snapshot.val();
-        var key = snapshot.key();
-        displayChatMessage(message.name, message.text, key);
-        ItemsAddedByOthers(message.time, message.text, key);
-      });
-
-
-
-
- groupRef.on('child_removed', function(snapshot) {
-        var message = snapshot.val();
-        var key = snapshot.key();
-        removeListItemUI(message.name, message.text, key);
- });
-
-
-
-
-
-      function displayChatMessage(name, text, key) {
-        $('<div/>').prepend($('<div class="listItem" onMouseDown="removeListItemDB(this.id)" id="'+ key +'">').text(text)).appendTo($('.listItems'));
-        $('.listItems')[0].scrollTop = $('.listItems')[0].scrollHeight;
-        $('</div>').text();
-      };
-
+      // remove item from db and listItems
       function removeListItemUI(name, text, key)
       {
        // $('.listItem').child(text).remove();
@@ -369,39 +234,53 @@ function getListUpdates(id)
       }
 
 
-      
-function doubletap(id) {
-
-//$("#" + id).on("tap",function(){
-
-
-      console.log(userGroup + " --V-- " + id);
-        var deleteRef = new Firebase("https://todofyp.firebaseio.com/listItems/" + userGroup + "/" + id);
-        deleteRef.remove();
-
-
-//});
-}
-
-
-
-
-
-
 
 
             function removeListItemDB(id)
       {
 
- doubletap(id);
+      console.log(userGroup + " --V-- " + id);
+        var deleteRef = new Firebase("https://todofyp.firebaseio.com/listItems/" + userGroup + "/" + id);
+        deleteRef.remove();
 
-}
+      }
+
+
+
+
+      //check when user online
+      //alert user when item set to urgent
+
+ //alert user when item set to urgent
+ 
+
+ // Attach an asynchronous callback to read the data at our posts reference
+ myDataRef.on("child_changed", function(snapshot) {
+   var itemStatus = snapshot.key();
+   if (groupString.indexOf(itemStatus) >= 0)
+ {
+  var groupUrgentRef = new Firebase('https://todofyp.firebaseio.com/listItems/' + itemStatus);
+  groupUrgentRef.on("child_changed", function(snapshot) {
+   var itemStatus = snapshot.val();
+   var hitmebaby = itemStatus.status;
+   if(hitmebaby != 'nrl')
+   {
+   alert('Item marked at urgent');
+ }
+ })
+   }
+ }, function (errorObject) {
+   console.log("The read failed: " + errorObject.code);
+ });
 
 
 
 
 
-var amOnline = new Firebase('https://todofyp.firebaseio.com/.info/connected');
+
+ //items added while offline
+
+ var amOnline = new Firebase('https://todofyp.firebaseio.com/.info/connected');
 var userRef = new Firebase('https://todofyp.firebaseio.com/presence/' + userID);
 amOnline.on('value', function(snapshot) {
   if (snapshot.val()) {
@@ -409,7 +288,6 @@ amOnline.on('value', function(snapshot) {
     //userRef.set(true);
   }
 });
-
 
 function ItemsAddedByOthers(timeAdded, text, key)
 {
@@ -434,17 +312,3 @@ userRef.on('value', function(snapshot) {
 
 
 }
-
-
-
- $('#createGroup').keypress(function (e) {
-        if (e.keyCode == 13) {
-            createGroup();
-            $('#createGroup').val('');
-}
-})
-
-}
-
-
-*/
